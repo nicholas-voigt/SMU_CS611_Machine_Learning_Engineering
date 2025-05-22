f2f5f1c30fde45cca4f354640be53463d29dd2e2
nicholas-voigt <nicovo215@googlemail.com>
2025-05-22 08:04:52 +0000

    changed load_data function to handle csv and parquet files

--- assignment_1/utils/data_loading.py
+++ assignment_1/utils/data_loading.py
@@ -27,16 +27,40 @@ def load_data(spark: SparkSession, input_directory: str, partition: str | None) 
         file_path = os.path.join(input_directory, partition)
         if not os.path.exists(file_path):
             raise FileNotFoundError(f"File for partition {partition} does not exist in {input_directory}.")
-        # Load the data from the CSV file
-        df = spark.read.csv(file_path, header=True, inferSchema=True)
+        # Load CSV or parquet file based on the file extension
+        if file_path.endswith('.parquet'):
+            df = spark.read.parquet(file_path)
+        elif file_path.endswith('.csv'):
+            df = spark.read.csv(file_path, header=True, inferSchema=True)
+        else:
+            raise ValueError(f"Unsupported file format for {file_path}. Only CSV and Parquet are supported.")
         print(f"Loaded data from {file_path}. Row count: {df.count()}")
         return df
     
     # If no partition is provided, load all data in the directory
     else:
-        all_files_path = os.path.join(input_directory, "*.csv")
-        print(f"Loading csv files with wildcard: {all_files_path}")
-        df = spark.read.csv(all_files_path, header=True, inferSchema=True)
+        # check which file types are in the directory
+        file_types = set()
+        for file in os.listdir(input_directory):
+            if file.endswith('.csv'):
+                file_types.add('csv')
+            elif file.endswith('.parquet'):
+                file_types.add('parquet')
+            else:
+                raise ValueError(f"Unsupported file format in {input_directory}. Only CSV and Parquet are supported.")
+        # If both file types are present, raise an error
+        if len(file_types) > 1:
+            raise ValueError(f"Multiple file types found in {input_directory}. Please specify a partition.")
         
-        print(f"Loaded data from all csv files in {input_directory}. Row count: {df.count()}")
+        # Load files based on the file type
+        if 'parquet' in file_types:
+            all_files_path = os.path.join(input_directory, "*.parquet")
+            print(f"Loading parquet files with wildcard: {all_files_path}")
+            df = spark.read.parquet(all_files_path)
+        else:
+            all_files_path = os.path.join(input_directory, "*.csv")
+            print(f"Loading csv files with wildcard: {all_files_path}")
+            df = spark.read.csv(all_files_path, header=True, inferSchema=True)
+        
+        print(f"Loaded data from all files from {input_directory}. Row count: {df.count()}")
         return df
